9bf81af0e7defcf2e036a48c9be195d1d0bb2d9d
berry <BerryWahlberg@gmail.com>
2026-01-13 22:10:27 +0800

    Fix thumbnail display issue by implementing custom value converters\n\n1. Created IsNullConverter and IsNotNullConverter classes\n2. Registered converters in App.axaml Resources\n3. Updated MainWindow.axaml to use custom converters\n4. Updated ImportWizardView.axaml to use custom converters\n5. Fixed ObjectConverters.IsNull references that were causing binding errors\n\nThis commit resolves the thumbnail display issue by implementing proper value converters for null checking in Avalonia bindings.

---
+++ src/Presentation/Converters/ValueConverters.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace BerryAIGCToolbox.Converters;
+
+public class IsNullConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value == null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class IsNotNullConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value != null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
