938e73f42ef7078c9d302a64652c0101b6834a3d
Deepseek <BerryWahlberg@gmil.com>
2026-01-22 13:45:46 +0800

    Phase 5: Fixed XAML files and added code-behind files

--- src/Presentation/Views/MainWindow.axaml.cs
+++ src/Presentation/Views/MainWindow.axaml.cs
@@ -1,11 +1,26 @@
 using Avalonia.Controls;
 
-namespace BerryAIGCToolbox.Views;
+namespace BerryAIGC.Toolkit.Views;
 
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void MinimizeButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    public void MaximizeButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+    }
+
+    public void CloseButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        Close();
     }
 }
---
+++ src/Presentation/Views/ModelsWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace BerryAIGC.Toolkit.Views;
+
+public partial class ModelsWindow : Window
+{
+    public ModelsWindow()
+    {
+        InitializeComponent();
+    }
+}
